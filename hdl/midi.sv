2d805ca85ca7ecba92d1d50223bf8aed298363d6
Andrew Li <andrewli357@gmail.com>
2023-11-24 17:01:49 -0500

    Add pitchbend support and start on mod vibrato

--- hdl/midi.sv
+++ hdl/midi.sv
@@ -18,13 +18,39 @@ module midi
   localparam PITCH_BEND = 8'hE0;
   localparam CC         = 8'hB0;
 
+  localparam MOD_WHEEL     = 8'h01;
+  // MPK mini 3 wheels
+  localparam ATTACK_TIME   = 8'h46;
+  localparam DECAY_TIME    = 8'h47;
+  localparam SUSTAIN_LEVEL = 8'h48;
+  localparam RELEASE_TIME  = 8'h49;
+
   logic [SYNTH_PHASE_ACC_BITS-1:0] NOTES [107:0];
   initial $readmemb(`FPATH(notes.mem), NOTES);
 
-  logic [3:0] notes_on;
+  logic [31:0] PITCH_BEND_FACTORS [127:0];
+  initial $readmemb(`FPATH(pitchbends.mem), PITCH_BEND_FACTORS);
+
   logic [7:0] pitch; // just support one voice at a time
+  logic [7:0] pitchbend;
   logic [7:0] velocity;
-  assign phase_incr_out = pitch ? NOTES[pitch - 12] : 0; // since index 0 is B0, which is MIDI note 12
+  logic [7:0] mod;
+
+  logic [SYNTH_PHASE_ACC_BITS-1:0] lfo_phase_incr;
+  assign lfo_phase_incr = 
+
+  logic [SYNTH_WIDTH-1:0] lfo_out;
+
+  triangle lfo(
+    .clk_in(clk_in),
+    .rst_in(rst_in),
+    .phase_incr_in(lfo_phase_incr),
+    .val_out(lfo_out)
+  );
+
+  logic [63:0] temp;
+  assign temp = NOTES[pitch - 12] * PITCH_BEND_FACTORS[pitchbend];
+  assign phase_incr_out = pitch ? temp >>> 20 : 0; // since index 0 is B0, which is MIDI note 12
 
   logic [MIDI_BYTES-1:0] prev_event;
   logic new_event;
@@ -33,21 +59,36 @@ module midi
   always_ff @(posedge clk_in) begin
     if (rst_in) begin
       pitch <= 0;
-      notes_on <= 0;
+      pitchbend <= 64;
     end else if (new_event) begin
       case (midi_event[23:16])
         NOTE_ON: begin
           pitch <= midi_event[15:8];
           velocity <= midi_event[7:0];
-          notes_on <= notes_on + 1;
         end
         NOTE_OFF: begin
-          notes_on <= notes_on - 1;
-          // only turn off synth when last note is release
-          if (notes_on == 1) begin
+          // only turn off synth when current note is released
+          if (midi_event[15:8] == pitch) begin
             pitch <= 0;
-            velocity <= 0;
+            velocity <= midi_event[7:0];
           end
+        end
+        PITCH_BEND: begin
+          // Use MSBs of pitchbend event
+          pitchbend <= midi_event[6:0];
+        end
+        CC: begin
+          case (midi_event[15:8])
+            MOD_WHEEL: begin
+              mod <= midi_event[6:0];
+            end
+            ATTACK_TIME: begin
+
+            end
+            RELEASE_TIME: begin
+
+            end
+          endcase
         end
       endcase
     end
--- scripts/phasegen.py
+++ scripts/phasegen.py
@@ -10,5 +10,16 @@ phases = map(lambda freq: f'{round(freq / resolution):024b}', freqs)
 with open('../data/notes.mem', 'w') as f:
   f.write('\n'.join(phases))
 
+# also generate pitch bend factors
+# pitch bend is 14-bit in MIDI but we'll only take the top 7 bits
+# where 64 is no bend, 0 is max lowbend, 127 is max highbend
+# and then map them to +-2 semitone range, i.e.
+# bend = (x / 64) - 1
+# factor = 2^(2 * bend / 12)
+# then we'll used fixed-point arithmetic with 2^20 scaling and
+# express them as 32-bit numbers
+PITCHBEND_WIDTH = 7
 
-
+pitchbend_factors = map(lambda x: f'{round(2 ** ((2 * (x / 64 - 1) / 12) + 20)):032b}', range(128))
+with open('../data/pitchbends.mem', 'w') as f:
+  f.write('\n'.join(pitchbend_factors))
